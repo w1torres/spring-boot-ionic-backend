adc2f513651e2e836e35672035346ea6092e29c1
Wildes Torres <wildestorres@gmail.com>
2022-09-03 17:21:43 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/wildessilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/wildessilva/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.wildessilva.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,7 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo" + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 				
 	}
 		
@@ -42,6 +43,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possuí produtos.");
 		}
 		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 	
 }
