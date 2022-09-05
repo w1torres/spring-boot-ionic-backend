aa6dab61d9a16bef41e1465f0dbffcba81447191
Wildes Torres <wildestorres@gmail.com>
2022-09-04 21:45:08 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/wildessilva/cursomc/domain/Cliente.java
+++ src/main/java/com/wildessilva/cursomc/domain/Cliente.java
@@ -15,9 +15,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.wildessilva.cursomc.domain.enums.TipoCliente;
 
 @Entity
--- src/main/java/com/wildessilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/wildessilva/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wildessilva.cursomc.domain.Categoria;
+import com.wildessilva.cursomc.dto.CategoriaDTO;
 import com.wildessilva.cursomc.repositories.CategoriaRepository;
 import com.wildessilva.cursomc.services.exceptions.DataIntegrityException;
 import com.wildessilva.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 			return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
