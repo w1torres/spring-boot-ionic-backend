4ccdc87dad9209cc81b0c5edba9752702238d337
Wildes Torres <wildestorres@gmail.com>
2022-09-14 23:14:03 -0300

    Inserindo um novo cliente com o método POST

--- src/main/java/com/wildessilva/cursomc/resources/ClienteResource.java
+++ src/main/java/com/wildessilva/cursomc/resources/ClienteResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wildessilva.cursomc.domain.Cliente;
 import com.wildessilva.cursomc.dto.ClienteDTO;
+import com.wildessilva.cursomc.dto.ClienteNewDTO;
 import com.wildessilva.cursomc.services.ClienteService;
 
 @RestController
@@ -35,6 +36,14 @@ public class ClienteResource {
 			
 	}
 			
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();	
+	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
--- src/main/java/com/wildessilva/cursomc/services/ClienteService.java
+++ src/main/java/com/wildessilva/cursomc/services/ClienteService.java
@@ -9,10 +9,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.wildessilva.cursomc.domain.Cidade;
 import com.wildessilva.cursomc.domain.Cliente;
+import com.wildessilva.cursomc.domain.Endereco;
+import com.wildessilva.cursomc.domain.enums.TipoCliente;
 import com.wildessilva.cursomc.dto.ClienteDTO;
+import com.wildessilva.cursomc.dto.ClienteNewDTO;
+import com.wildessilva.cursomc.repositories.CidadeRepository;
 import com.wildessilva.cursomc.repositories.ClienteRepository;
+import com.wildessilva.cursomc.repositories.EnderecoRepository;
 import com.wildessilva.cursomc.services.exceptions.DataIntegrityException;
 import com.wildessilva.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -22,11 +29,25 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo" + Cliente.class.getName()));
 			
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -35,8 +56,7 @@ public class ClienteService {
 		return repo.save(obj);
 	}
 	
-	
-	
+		
 	public void delete(Integer id) {
 		find(id);
 		try {
@@ -58,6 +78,21 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);	
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
