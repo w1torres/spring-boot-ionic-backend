16aa2f9d99302b610d54de8ef65f084c6c811868
Wildes Torres <wildestorres@gmail.com>
2022-09-14 22:16:47 -0300

    Correcoes: update CategoriaService e construtor Pagamento

--- src/main/java/com/wildessilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/wildessilva/cursomc/services/CategoriaService.java
@@ -35,7 +35,8 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
 		return repo.save(obj);
 	}
 	
@@ -60,6 +61,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 	
 }
