5ab9a0c51ecc736a902a9fbb1f1052de11f80304
Wildes Torres <wildestorres@gmail.com>
2022-09-15 23:18:01 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/wildessilva/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/wildessilva/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.wildessilva.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.wildessilva.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepositoryImplementation<Cliente, Integer>{
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
