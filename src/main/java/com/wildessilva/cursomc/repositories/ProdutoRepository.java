6608188fc90e57debffe6d2cb54d425bf7e3fd59
Wildes Torres <wildestorres@gmail.com>
2022-10-01 18:30:51 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/wildessilva/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/wildessilva/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,21 @@
 package com.wildessilva.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.wildessilva.cursomc.domain.Categoria;
 import com.wildessilva.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepositoryImplementation<Produto, Integer>{
-
-}
+	
+	
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+ }
