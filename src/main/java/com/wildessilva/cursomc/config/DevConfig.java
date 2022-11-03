a3acd2e905f59967b82cd1dde680c8573c00ffd3
Wildes Torres <wildestorres@gmail.com>
2022-11-03 06:17:07 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/wildessilva/cursomc/config/DevConfig.java
+++ src/main/java/com/wildessilva/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.wildessilva.cursomc.services.DBService;
+import com.wildessilva.cursomc.services.EmailService;
+import com.wildessilva.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		
 		dbService.instatiateDataBase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+	    return new SmtpEmailService();
 	}
 }
