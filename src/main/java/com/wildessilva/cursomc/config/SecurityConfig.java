28be1ef66df0bc7347e6358045bf5da4b17c7d81
Wildes Torres <wildestorres@gmail.com>
2022-11-10 08:49:09 -0300

    Implementando autenticação e geração do token jwt

--- src/main/java/com/wildessilva/cursomc/config/SecurityConfig.java
+++ src/main/java/com/wildessilva/cursomc/config/SecurityConfig.java
@@ -7,19 +7,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@SuppressWarnings("deprecation")
+import com.wildessilva.cursomc.security.JWTAuthenticationFilter;
+import com.wildessilva.cursomc.security.JWTUtil;
+
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
 
     @Autowired
     private Environment env;
+    
+    @Autowired
+    private JWTUtil jwtUtil;
+    
+    @Autowired
+    private UserDetailsService userDetailsService;
     
     public static final String[] PUBLIC_MATCHERS = {
             "/h2-console/**"          
@@ -43,13 +57,24 @@ public class SecurityConfig{
             .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
             .antMatchers(PUBLIC_MATCHERS).permitAll()
             .anyRequest().authenticated();
+        http.addFilter(new JWTAuthenticationFilter(authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)), jwtUtil));
+        //http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }    
     
+    public void  configure(AuthenticationManagerBuilder auth) throws Exception  {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+    
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+         return authenticationConfiguration.getAuthenticationManager();
     }
 }
 
