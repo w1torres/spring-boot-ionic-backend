46f16f980c978543b4fea1d04d30f68385392813
Wildes Torres <wildestorres@gmail.com>
2022-11-23 07:20:58 -0300

    Esqueci a senha

--- src/main/java/com/wildessilva/cursomc/config/SecurityConfig.java
+++ src/main/java/com/wildessilva/cursomc/config/SecurityConfig.java
@@ -47,7 +47,8 @@ public class SecurityConfig {
     
 
     public static final String[] PUBLIC_MATCHERS_POST = {
-            "/clientes/**"           
+            "/clientes/**",
+            "/auth/forgot/**"
     };
 
     
@@ -60,7 +61,7 @@ public class SecurityConfig {
         
         http.cors().and().csrf().disable();
         http.authorizeRequests()
-            .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_POST).permitAll()
+            .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
             .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
             .antMatchers(PUBLIC_MATCHERS).permitAll()
             .anyRequest().authenticated();
