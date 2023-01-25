be1a40dbded648a6255c2f5f862b62562e95b350
Wildes Torres da Silva <wildes.silva@serpro.gov.br>
2023-01-25 07:01:19 -0300

    Padronizando formatos de excessões

--- src/main/java/com/wildessilva/cursomc/resources/exception/ValidationError.java
+++ src/main/java/com/wildessilva/cursomc/resources/exception/ValidationError.java
@@ -6,19 +6,17 @@ import java.util.List;
 public class ValidationError extends StandardError {
 	private static final long serialVersionUID = 1L;
 	
-	private List<FieldMessage> errors = new ArrayList<>();
-
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
-	}
+	private List<FieldMessage> errors = new ArrayList<>();	
 	
-	public List<FieldMessage> getErrors(){
+	public ValidationError(Long timeStamp, Integer status, String error, String message, String path) {
+        super(timeStamp, status, error, message, path);
+    }
+
+    public List<FieldMessage> getErrors(){
 		return errors;
 	}
 	
 	public void addError(String fieldName, String messagem) {
 		errors.add(new FieldMessage(fieldName, messagem));
 	}	
-	
-
 }
